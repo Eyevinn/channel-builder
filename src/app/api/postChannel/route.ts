c18c18be80aa30b085554c30905bddb97113a5f1
Filip Hedman <64984750+JinHedman@users.noreply.github.com>
2024-10-02 08:52:19 +0200

    Channel builder merge 1 (#1)
    * feat: setup
    
    * feat: implemented Eyevinn Channel Engine and pages to display streams for channels
    
    * fix(backend): changed backend to use OSAAS channel-engine
    
    * feat(fix): added page and API route
    
    added opts in postChannels request header for playlist channels, so they function properly.
    improved the way streams and channels are fetched.
    this ensures better handling of playlist type channels.
    
    * feat: add design for channel viewer with grid and back button
    
    - Added a 4x4 grid to be filled with channels
    - Added back button for navigation
    - Still needs testing for mobile & channel loading performance for many channels
    
    * fix: fetches new token regularly
    
    * fix: changed some css on the channel-viewer
    
    * fix: no-cache on fetch token
    
    * style: run prettier on project
    
    ---------
    
    Co-authored-by: Edvin Hedenström <edvin.hedenstrom1@gmail.com>

---
+++ src/app/api/postChannel/route.ts
@@ -0,0 +1,46 @@
+import { NextResponse } from 'next/server';
+import {
+  ensureValidServiceToken,
+  fetchServiceToken
+} from '../../lib/serviceToken'; // service token handler
+
+// define payload type for the POST request
+interface ChannelPayload {
+  name: string;
+  type: string;
+  url: string;
+}
+
+const API_URL = 'https://api-ce.prod.osaas.io/channel';
+
+export async function POST(req: Request) {
+  try {
+    // check if token is valid, else generate a new one
+    const serviceToken = await fetchServiceToken();
+    //console.log('Service token:', serviceToken);
+
+    const body: ChannelPayload = await req.json();
+    console.log('Channel payload:', body);
+
+    // POST request to OSAAS to create channel in FAST Channel Engine
+    const response = await fetch(API_URL, {
+      method: 'POST',
+      headers: {
+        accept: 'application/json',
+        'x-jwt': `Bearer ${serviceToken}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    });
+
+    const data = await response.json();
+
+    return NextResponse.json(data, { status: response.status });
+  } catch (error) {
+    console.error('Error creating channel:', error);
+    return NextResponse.json(
+      { error: 'Failed to create channel' },
+      { status: 500 }
+    );
+  }
+}
